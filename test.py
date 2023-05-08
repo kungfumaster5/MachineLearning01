7bc93f3c9cbdb26b07cfe7b22830b91fad54c0e6
kungfumaster5 <1922800024@qq.com>
2023-05-08 17:49:23 +0800

    Changes

--- test.py
+++ test.py
@@ -1,8 +1,1 @@
-import matplotlib.pyplot as plt
-plt.title("标题")
-# 解决中文标题乱码问题
-plt.rcParams['font.sans-serif']=['SimHei']
-plt.figure(figsize=(6, 3))
-plt.plot(6, 3)
-plt.plot(3, 3 * 2)
-plt.show()
+# 这是用来测试的文件
